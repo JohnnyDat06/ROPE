14673107276322254046ec284334a0ec892cc92e
JohnnyDat06 <hotandat4321@gmail.com>
2026-02-05 09:04:31 +0700

    Feat: Add strafe animations, bone fixer & rig relay
    Finish equip and holster weapon smooth between back with AKX, AK47 and hip with Pistol

--- Assets/_Game/Scripts/Gun/ActiveWeapon.cs
+++ Assets/_Game/Scripts/Gun/ActiveWeapon.cs
@@ -1,30 +1,38 @@
 ﻿using StarterAssets;
+using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
-using UnityEditor.Animations;
+using static RaycastWeapon;
 
 public class ActiveWeapon : MonoBehaviour
 {
     public Transform crossHairTarget;
-    private bool isAiming = false;
+    public bool IsHolstered { get; private set; } = true;
 
     [Header("Weapon")]
     [SerializeField] private Transform weaponLeftGrip;
     [SerializeField] private Transform weaponRightGrip;
-    public Transform weaponParent;
+    public Transform weaponParent; 
     public Animator rigController;
+
+    [Header("Holster Sockets (Body)")]
+    public Transform backSocket;
+    public Transform hipSocket;
+
+    [Header("Settings")]
+    public float fireDelayTime = 0.5f;
 
     private RaycastWeapon weapon;
     private StarterAssetsInputs _input;
+    private float _nextFireTime = 0f;
+    private bool _wasHolstered = true;
 
     private void Start()
     {
-        // Use AnimatorUpdateMode.Fixed instead of the obsolete AnimatePhysics, make sure weights are set to 1 on rig constraints
         rigController.updateMode = AnimatorUpdateMode.Fixed;
-        rigController.cullingMode = AnimatorCullingMode.CullUpdateTransforms;
         rigController.cullingMode = AnimatorCullingMode.AlwaysAnimate;
-        rigController.updateMode = AnimatorUpdateMode.Normal;
         rigController.animatePhysics = true;
+        rigController.updateMode = AnimatorUpdateMode.Normal;
 
         _input = GetComponent<StarterAssetsInputs>();
         RaycastWeapon existingWeapon = GetComponentInChildren<RaycastWeapon>();
@@ -37,44 +45,96 @@ public class ActiveWeapon : MonoBehaviour
         ShootControl();
     }
 
-    public bool IsAiming() => isAiming;
     private void ShootControl()
     {
         if (weapon)
         {
-            isAiming = true;
-            if (_input.shoot)
-            {
-                if (!weapon.isFiring)
-                    weapon.StartFiring();
+            IsHolstered = rigController.GetBool("holster_weapon");
 
+            if (_wasHolstered == true && IsHolstered == false)
+            {
+                _nextFireTime = Time.time + fireDelayTime;
+            }
+            _wasHolstered = IsHolstered;
+
+            if (_input.shoot && !IsHolstered && Time.time >= _nextFireTime)
+            {
+                if (!weapon.isFiring) weapon.StartFiring();
                 weapon.UpdateFiring(Time.deltaTime);
             }
             else
-                if (weapon.isFiring)
-                weapon.StopFiring();
+            {
+                if (weapon.isFiring) weapon.StopFiring();
+            }
 
             if (_input.holster)
             {
-                bool isHolstering = rigController.GetBool("holster_weapon");
-                rigController.SetBool("holster_weapon", !isHolstering);
+                bool currentHolsterState = rigController.GetBool("holster_weapon");
+                rigController.SetBool("holster_weapon", !currentHolsterState);
                 _input.holster = false;
             }
-
             weapon.UpdateBullets(Time.deltaTime);
+        }
+        else
+        {
+            IsHolstered = true;
         }
     }
 
     public void Equip(RaycastWeapon newWeapon)
     {
-        if (weapon)
-            Destroy(weapon.gameObject);
+        if (weapon) Destroy(weapon.gameObject);
 
         weapon = newWeapon;
         weapon.raycastDestination = crossHairTarget;
-        weapon.transform.parent = weaponParent;
+
+        weapon.transform.SetParent(weaponParent);
         weapon.transform.localPosition = Vector3.zero;
         weapon.transform.localRotation = Quaternion.identity;
+
         rigController.Play("equip_" + weapon.weaponName);
+        rigController.SetBool("holster_weapon", false);
+        IsHolstered = false;
+        _wasHolstered = true;
+    }
+
+    public void OnWeaponEquip()
+    {
+        if (weapon != null)
+        {
+            weapon.transform.SetParent(weaponParent);
+            weapon.transform.localPosition = Vector3.zero;
+            weapon.transform.localRotation = Quaternion.identity;
+        }
+    }
+
+    public void OnWeaponHolster()
+    {
+        if (weapon == null) return;
+
+        Transform targetSocket = null;
+
+        // Kiểm tra xem khẩu súng hiện tại muốn về đâu
+        switch (weapon.holsterLocation)
+        {
+            case HolsterLocation.Back:
+                targetSocket = backSocket;
+                break;
+            case HolsterLocation.Hip:
+                targetSocket = hipSocket;
+                break;
+        }
+
+        // Thực hiện chuyển cha (Parent)
+        if (targetSocket != null)
+        {
+            weapon.transform.SetParent(targetSocket);
+            weapon.transform.localPosition = Vector3.zero;
+            weapon.transform.localRotation = Quaternion.identity;
+        }
+        else
+        {
+            Debug.LogError("Chưa gán Socket cho vị trí này trong Inspector!");
+        }
     }
 }
---
+++ Assets/_Game/Scripts/Gun/RigEventRelay.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class RigEventRelay : MonoBehaviour
+{
+    private ActiveWeapon activeWeapon;
+
+    private void Start()
+    {
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
+
+        if (activeWeapon == null)
+        {
+            Debug.LogError("Không tìm thấy script ActiveWeapon ở object cha!");
+        }
+    }
+
+    public void TriggerEquip()
+    {
+        if (activeWeapon != null)
+        {
+            activeWeapon.OnWeaponEquip();
+        }
+    }
+
+    public void TriggerHolster()
+    {
+        if (activeWeapon != null)
+        {
+            activeWeapon.OnWeaponHolster();
+        }
+    }
+}
